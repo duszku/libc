8f5e271cc5f7293f33d12d6153805e331937d805
duszku <86882994+duszku@users.noreply.github.com>
2023-05-22 01:27:01 +0200

    Declared `stpncpy()`

--- include/amd64string.h
+++ include/amd64string.h
@@ -1,6 +1,9 @@
 #ifndef AMD64STRING_H
 #define AMD64STRING_H
 
+#include <stddef.h>
+
 extern char     *stpcpy(char *, const char *);
+extern char     *stpncpy(char *, const char *, size_t);
 
 #endif /* AMD64STRING_H */

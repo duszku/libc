655e42465d1ac86df41ad64a21812f97bd33f350
duszku <86882994+duszku@users.noreply.github.com>
2023-05-22 01:31:33 +0200

    Created `stpncpy.s`

---
+++ src/stpncpy.s
@@ -0,0 +1,15 @@
+        section .text
+        global  stpncpy
+
+;-------------------------------------------------------------------------------
+; Function: stpncpy
+; Purpose:  Copies a string up to a given length and returns pointer to its end
+; Inputs:
+;   - address s1: points to the destination
+;   - address s2: points to the source
+;   - uint    n:  maximal number of characters to copy
+; Output:   Address of the nul character in the destination string
+;-------------------------------------------------------------------------------
+
+stpncpy:
+        ret

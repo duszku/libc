53430ccc824586d43f28e653d7e331543262ff81
duszku <86882994+duszku@users.noreply.github.com>
2023-05-23 01:07:16 +0200

    Created `strcat.s`

---
+++ src/strcat.s
@@ -0,0 +1,14 @@
+        section .text
+        global  strcat:function
+
+;-------------------------------------------------------------------------------
+; Function: strcat
+; Purpose:  Concatenates two strings
+; Inputs:
+;   - address s1: points to the destination
+;   - address s2: points to the source
+; Output:   s1
+;-------------------------------------------------------------------------------
+
+strcat:
+        ret

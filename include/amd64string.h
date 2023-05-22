07988c6c914a7d5da4c816ea695b10cc4865569d
duszku <86882994+duszku@users.noreply.github.com>
2023-05-23 01:06:27 +0200

    Declared `strcat()`

--- include/amd64string.h
+++ include/amd64string.h
@@ -5,5 +5,6 @@
 
 extern char     *stpcpy(char *, const char *);
 extern char     *stpncpy(char *, const char *, size_t);
+extern char     *strcat(char *, const char *);
 
 #endif /* AMD64STRING_H */

a605a72e5fe9ba3377d25b380b9d2dc807987bf0
duszku <86882994+duszku@users.noreply.github.com>
2023-05-26 00:18:39 +0200

    Implemented `memccpy()`

--- src/memccpy.s
+++ src/memccpy.s
@@ -9,8 +9,33 @@
 ;   - address s2: points to the source
 ;   - uchar   c:  when c is met, copying stops
 ;   - uint    n:  even if c does not occur, at most n copies will take place
-; Output:   Address of the nul character in the destination string
+; Output:   Pointer to copy of c in s1 or null if no c occurs in s1
 ;-------------------------------------------------------------------------------
 
 memccpy:
+        mov     rbx, rdi        ; store destination base pointer in rbx
+        add     rbx, rcx        ; calculate address of the last accessible byte 
+
+    ; copying loop
+    lopcpy:
+        mov     al, byte [rsi]  ; move offsetted byte to al
+        cmp     al, dl          ; check if its not equal to c
+        je      endc            ; end if it is
+
+        mov     [rdi], al       ; copy moved byte
+        inc     rdi             ; increment destination offset
+        inc     rsi             ; increment source offset
+
+        cmp     rdi, rbx        ; check if final byte was reached
+        jl      lopcpy          ; continue if not
+
+    ; cause of end: reached final byte
+    endn:
+        mov     rax, 0          ; return NULL
+        ret
+
+    ; cause of end: found character c
+    endc:
+        mov     rax, rbx
+        sub     rax, 1
         ret
